e7a3c68a5bb9f988638639ed598bda5133a8b108
Aidan Astridge <115724380+aidanastridge@users.noreply.github.com>
2023-08-08 15:56:26 -0400

    Add files via upload

---
+++ wideloop/wideloop.R
@@ -0,0 +1,19 @@
+library(tidyverse)
+
+#EXAMPLE
+
+#read and drop columns that are not values
+df <- read.csv("/Users/aidanastridge/Documents/32100353.csv", stringsAsFactors = TRUE)
+df <- select(df, -c('DGUID','UOM','UOM_ID','SCALAR_FACTOR','SCALAR_ID','VECTOR','COORDINATE','STATUS','SYMBOL','TERMINATED','DECIMALS')) 
+
+#pivot wide
+df <- df %>%
+  pivot_wider(names_from = Estimates, values_from = VALUE)
+
+#create seperate datasets with for loop
+for (x in levels(df$GEO)) {
+  a <- df  
+  b <- subset(a, GEO  == x)
+  assign(paste0(x), b, envir = .GlobalEnv)}
+
+
